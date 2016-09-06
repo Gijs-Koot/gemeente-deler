de6df6916bc104c9ed2477bb2e9732c563aab8b7
Gijs Koot <gijskoot@gmail.com>
2016-09-06 22:25:45 +0200

    add map example

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 console.log("Welcome!");
 
-var app = angular.module('gd-app', ['ngRoute', 'ui.bootstrap']);
+var app = angular.module('gd-app', ['ngRoute', 'ui.bootstrap', 'rzModule', 'leaflet-directive']);
 
 app.config(function($routeProvider) {
     $routeProvider.when('/about', {
@@ -8,13 +8,59 @@ app.config(function($routeProvider) {
             controller  : function(){
 
             }
+        }).when('/ui-test', {
+            templateUrl: 'partials/ui-test.html',
+            controller: function($scope){
+                console.log("Welcome to home.");
+                $scope.checkModel = {
+                  wot: "wot"
+                };
+                $scope.verticalSlider = {
+                  value: 4,
+                  options: {
+                    floor: 0,
+                    ceil: 10,
+                    vertical: true
+                  }
+                };
+              }
+        }).when('/map-test', {
+            templateUrl: 'partials/map-test.html',
+            controller: "GeoJSONController"
         }).otherwise({
                 templateUrl : 'partials/home.html',
                 controller  : function($scope){
-                    console.log("Welcome to home.");
-                    $scope.checkModel = {
-                      wot: "wot"
-                    };
+
                 }
         })
 });
+
+app.controller("GeoJSONController", ['$scope', '$http', function($scope, $http) {
+    angular.extend($scope, {
+        japan: {
+            lat: 37.26,
+            lng: 138.86,
+            zoom: 4
+        },
+        defaults: {
+            scrollWheelZoom: false
+        }
+    });
+
+    // Get the countries geojson data from a JSON
+    $http.get("data/JPN.geo.json").success(function(data, status) {
+        angular.extend($scope, {
+            geojson: {
+                data: data,
+                style: {
+                    fillColor: "green",
+                    weight: 2,
+                    opacity: 1,
+                    color: 'white',
+                    dashArray: '3',
+                    fillOpacity: 0.7
+                }
+            }
+        });
+    });
+}]);
