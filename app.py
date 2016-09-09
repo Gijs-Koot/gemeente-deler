f3a8a03aa781bbc3637201426a6fc55f572d0e69
Gijs Koot <gijskoot@gmail.com>
2016-09-09 11:01:34 +0200

    add dummy endpoints

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__, static_url_path='')
 
-#@app.route('/<path:path>')
-#def static(path):
-#    return send_from_directory('static', path)
+@app.route('/similar')
+def similar():
+    return jsonify({
+        "results": ["G04", "G10"]
+    })
+
+@app.route('/datasets')
+def datasets():
+    return jsonify({
+        "results": [{
+            "name": "Criminaliteitindex",
+            "year": 2016,
+            "id": "crim_index_2016"
+        }, {
+            "name": "Bevolking",
+            "id": "count_2016",
+            "year": 2016
+        }]
+    })
+
+@app.route('/gemeentes')
+def gemeentes():
+    return jsonify({
+        "results": [{
+            "id": "G04",
+            "name": "Arnhem"
+        }, {
+            "id": "G10",
+            "name": "Nijmegen"
+        }]
+    })
+
+@app.route('/gemeente')
+def gemeente():
+    return jsonify({
+        "result": {
+            "name": "Arnhem",
+            "id": "G04",
+            "crimality": .4,
+            "count": 123
+        }
+    })
+
 
 if __name__ == "__main__":
 
