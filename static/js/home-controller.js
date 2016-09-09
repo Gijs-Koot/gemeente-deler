7dca47345afb8eedeaf7810cf4d8c16f499fb9c6
Gina Goldberg <gina.goldberg@hotmail.com>
2016-09-09 13:48:30 +0200

    display list of similar gemeentes

--- static/js/home-controller.js
+++ static/js/home-controller.js
@@ -5,4 +5,17 @@ app.controller("HomeController", ['$scope', '$http', function($scope, $http) {
       $scope.results = response.data.results;
     });
 
+    $scope.search_similar = function(){
+        console.log('Zoeken');
+        console.log($scope.selected_gemeente);
+
+        $http.post('/similar', {
+            "gemeente_id": $scope.selected_gemeente,
+            "categories": ["count_2015", "count_2014"]
+        }).then(function(response){
+          $scope.similar = response.data;
+        });
+
+    }
+
 }]);
