e3e063665cff5e9ce5a21a1effcbbbb567ee8eeb
Willem <willem.don@vodafone.com>
2016-09-09 16:20:51 +0200

    add extra columns and checkboxes

--- static/js/home-controller.js
+++ static/js/home-controller.js
@@ -5,14 +5,27 @@ app.controller("HomeController", ['$scope', '$http', function($scope, $http) {
       $scope.results = response.data.results;
     });
 
+    $scope.datasets = [];
+
+    $http.get('/datasets').then(function(response){
+        $scope.datasets = response.data;
+    });
 
     $scope.search_similar = function(){
         console.log('Zoeken');
         console.log($scope.selected_gemeente);
 
+        var categories = [];
+
+        $scope.datasets.map(function(dataset){
+            if(dataset.active){
+                categories.push(dataset.id);
+            }
+        });
+
         $http.post('/similar', {
             "gemeente_id": $scope.selected_gemeente,
-            "categories": ["aantal_verdachten"]
+            "categories": categories
         }).then(function(response){
 
           $scope.header = response.data.header;
