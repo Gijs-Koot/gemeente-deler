34606fdef62f00a8a337e7e19349dacae6b8b553
Gijs Koot <gijskoot@gmail.com>
2016-09-09 12:25:08 +0200

    add similarity endpoint

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, jsonify, request
 import feather
 import json
+from sklearn.neighbors import NearestNeighbors
+
+
 
 app = Flask(__name__, static_url_path='')
+df = feather.read_dataframe('./datasets/clean/clean_data.feather').dropna()
 
-df = feather.read_dataframe('./datasets/clean/clean_data.feather')
+nn = NearestNeighbors()
+
+def similar_gemeentes(gemeente_id, columns, n=5):
+
+    values = df[columns].values
+
+    nn.fit(values)
+    near = nn.kneighbors(df.loc[gemeente_id][columns].values.reshape(1, -1), n_neighbors=n)[1][0]
+
+    return df.ix[df.index[near]]
+
 with open('./datasets/clean/columns.json') as f:
     column_data = json.load(f)
 
@@ -14,13 +28,16 @@ def similar():
     params = request.json
     print(params)
 
-    gemeente_id = params["gemeente_id"]
+    gemeente_id = int(params["gemeente_id"])
     cats = params["categories"]
 
-    return jsonify({
-        "results": ["G04", "G10"],
-        "request": params
-    })
+    sim = similar_gemeentes(gemeente_id, cats)
+
+    results = [{
+        key: value for key, value in zip(["id"] + list(sim.columns), row.tolist())
+    } for row in sim.to_records()]
+
+    return jsonify(results)
 
 @app.route('/datasets')
 def datasets():
