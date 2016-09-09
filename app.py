d9a7b303bff0dab2c30340ac78ca55846450c40e
Willem <willem.don@vodafone.com>
2016-09-09 17:08:26 +0200

    add colors and format

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import json
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import pandas as pd
-
+import matplotlib
+from matplotlib import cm
 
 app = Flask(__name__, static_url_path='')
 df = pd.read_csv("./datasets/clean/clean_data.csv", index_col=0)
@@ -21,6 +22,24 @@ def similar_gemeentes(gemeente_id, columns, n=5):
 
 with open('./datasets/clean/columns.json') as f:
     column_data = json.load(f)
+
+import math
+
+
+def num_fmt(num):
+    i_offset = 15 # change this if you extend the symbols!!!
+    prec = 3
+    fmt = '.{p}g'.format(p=prec)
+    symbols = ['Y', 'T', 'G', 'M', 'k', '', 'm', 'u', 'n']
+
+    e = math.log10(abs(num))
+    if e >= i_offset + 3:
+        return '{:{fmt}}'.format(num, fmt=fmt)
+    for i, sym in enumerate(symbols):
+        e_thresh = i_offset - 3 * i
+        if e >= e_thresh:
+            return '{:{fmt}}{sym}'.format(num/10.**e_thresh, fmt=fmt, sym=sym)
+    return '{:{fmt}}'.format(num, fmt=fmt)
 
 @app.route('/similar', methods=["POST"])
 def similar():
@@ -40,21 +59,56 @@ def similar():
         key: value["name"] for key, value in column_data.items()
     }
 
-    print(sim.head())
-
     idx = sim.T.columns
     cols = sim.columns
 
-    header = [""] + sim.T.loc["regio", idx].values.tolist() + ["average"]
-    rows = sim.T.loc[list(set(cols) - {"regio"}), idx]
+    header = [""] + sim.T.loc["regio", idx].values.tolist()
+    rows = sim.T.loc[list(set(cols) - {"regio"}), idx].rename(rename)
 
-    rows["average"] = rows.apply(np.mean, axis=1)
+    rows_formatted = rows.reset_index().values.tolist()
 
-    rows = rows.rename(rename).reset_index().values.tolist()
+    from matplotlib.colors import rgb2hex
+
+    def get_color(value, max=1):
+
+        norm = matplotlib.colors.Normalize(vmin=-1 * max, vmax=max)
+        cmap = cm.ScalarMappable(norm=norm, cmap=matplotlib.cm.jet)
+        return rgb2hex(cmap.to_rgba(value))
+
+    for row in rows_formatted:
+
+        avg = row[-1]
+        diff = [(val - avg) for val in row[1:-1]]
+        max_diff = max([abs(d) for d in diff])
+
+
+        for index in range(len(row)):
+            value = row[index]
+
+            if type(value) == str:
+                row[index] = {
+                    "value": value,
+                    "color": None
+                }
+            else:
+
+                this_diff = value - avg
+
+                if value < 1:
+                    row[index] = {
+                        "value": "%.2f%%" % (float(value), ),
+                        "color": get_color(this_diff, max=max_diff)
+                    }
+
+                else:
+                    row[index] = {
+                        "value": num_fmt(value),
+                        "color": get_color(this_diff, max=max_diff)
+                    }
 
     return jsonify({
         "header": header,
-        "rows": rows
+        "rows": rows_formatted
     })
 
 @app.route('/datasets')
