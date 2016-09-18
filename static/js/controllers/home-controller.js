036c7f9929c73a5a54a6e995b69b40cf6eaccb8d
David Hildering <davidhildering@gmail.com>
2016-09-18 14:09:20 +0200

    set default if empty

--- static/js/controllers/home-controller.js
+++ static/js/controllers/home-controller.js
@@ -1,39 +1,49 @@
 app.controller("HomeController", ['$scope', '$http', function($scope, $http) {
     console.log("Welcome to home.");
 
+
+
     $http.get('api/gemeentes').then(function(response){
       $scope.results = response.data.results;
-        console.log("data", response.data);
-        console.log("results", response.data.results);
+        console.log('results', $scope.results);
+          $scope.selected_gemeente = 0;
     });
 
     $scope.datasets = [];
 
     $http.get('api/datasets').then(function(response){
         $scope.datasets = response.data;
+        console.log('datasets', $scope.datasets);
+        //temp workaround select value
+
     });
 
     $scope.search_similar = function(){
         console.log('Zoeken');
-        console.log($scope.selected_gemeente);
+
 
         var categories = [];
 
         $scope.datasets.map(function(dataset){
             if(dataset.active){
                 categories.push(dataset.id);
-                console.log('categories', categories);
             }
         });
+
+        if (categories.length == 0 ) {
+            categories.push($scope.datasets[0].id);
+            $scope.datasets[0].active = true;
+        }
 
         $http.post('api/similar', {
             "gemeente_id": $scope.selected_gemeente,
             "categories": categories
         }).then(function(response){
 
-          $scope.header = response.data.header;
+          $scope.similarResults = response.data;
+            console.log('similarResults', $scope.similarResults);
 
-          $scope.rows = response.data.rows;
+
 
 
         });
